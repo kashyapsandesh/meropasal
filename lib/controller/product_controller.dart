e663853fb5ce2b6aa032731ab00bddd0557415ef
kashyapsandesh <sandeshghimire202@gmail.com>
2023-05-13 11:50:42 +0545

    fetching subcategory form model using rootBundle

--- lib/controller/product_controller.dart
+++ lib/controller/product_controller.dart
@@ -1,5 +1,9 @@
+import 'package:flutter/services.dart';
 import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 
-class ProductController extends GetxController{
-// create a function to used json 
+class ProductController extends GetxController {
+// create a function to used json
+  getSubCategories() async {
+    var data = rootBundle.loadString("lib/services/category_model.json");
+  }
 }
